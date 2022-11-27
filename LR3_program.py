50b565202a9e0d732b9a9f20f12856e2a773e6a0
AlenK00 <alena.karpets02@mail.ru>
2022-11-27 16:57:06 +0400

    lab3

--- LR3_program.py
+++ LR3_program.py
@@ -28,12 +28,12 @@ mk=read_file("tab2(1).html")
 
 #pattern42 = re.compile(r"""(?:[a-zA-Z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x09\x0b\x0c\x0d\x21\x23-\x5b\x5d-\x7e]|\\[\x09\x0b\x0c\x0d])*")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9]+)?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|"(?:[\x09\x0b\x0c\x0d\x2d\x2e\x30-\x39\x41-\x5a\x61-\x7a]|\\[\x09\x0b\x0c])+")""")
 
-#pattern44 = re.compile(r"""(?:[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*(?:\-[a-zA-Z0-9]+)*(?:\_[a-zA-Z0-9]+)*)[a-zA-Z0-9]+@(?:[a-z]+\.)[a-z]{2,3}""")
+pattern44 = re.compile(r"""(?:[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*(?:\-[a-zA-Z0-9]+)*(?:\_[a-zA-Z0-9]+)*)[a-zA-Z0-9]+@(?:[a-z]+\.)[a-z]{2,3}""")
 
 #pattern44 = re.compile(r"""(?:[a-zA-Z0-9]+(?:[-._]?[a-zA-Z0-9]+)*)@(?:[a-z]+\.)[a-z]{2,3}""")
 
 
-pattern44 = re.compile(r"(?:[a-zA-Z0-9])+(?:[_\.\-])?(?:[a-zA-Z0-9])+\@[a-z]+\.[a-z]{2,3}");
+#pattern44 = re.compile(r"(?:[a-zA-Z0-9])+(?:[_\.\-])?(?:[a-zA-Z0-9])+\@[a-z]+\.[a-z]{2,3}");
 
 
 print ("\n-------список e-mail----------")
